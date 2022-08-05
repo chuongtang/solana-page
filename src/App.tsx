d05670f541b4f05fd05651dc606285f421d75600
chuongtang <chuong.tang@aol.com>
2022-08-05 11:40:30 -0600

    added Phantom symbol

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,32 @@ import React, { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 import { getProvider } from './utils'
-import NoPhantom from './components/NoPhantom'
+import NoPhantom from './components/NoPhantom';
+import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
+import { TLog } from '../types';
 
 // Constants
 const provider = getProvider();
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+export type ConnectedMethods =
+  | {
+      name: string;
+      onClick: () => Promise<string>;
+    }
+  | {
+      name: string;
+      onClick: () => Promise<void>;
+    };
+
+    interface Props {
+      publicKey: PublicKey | null;
+      connectedMethods: ConnectedMethods[];
+      handleConnect: () => Promise<void>;
+      logs: TLog[];
+      clearLogs: () => void;
+    }
 
 const App = () => {
 
--- src/components/NoPhantom.tsx
+++ src/components/NoPhantom.tsx
@@ -13,8 +13,8 @@ const NoPhantom = () => {
             </span>
           </h1>
 
-          <p className="max-w-xl mx-auto mt-4 sm:leading-relaxed sm:text-xl">
-            Please install the Phantom Wallet to continue
+          <p className="flex max-w-xl mx-auto justify-center mt-4 sm:leading-relaxed sm:text-xl">
+            Please install the <img src="https://phantom.app/img/phantom-logo.svg" alt="Phantom" className="mx-2 w-20"/>to continue
           </p>
 
           <div className="flex flex-wrap justify-center gap-4 mt-8">
