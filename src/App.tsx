e9061051a7cde685784ee6ac4d6fb723762073ec
chuongtang <chuong.tang@aol.com>
2022-08-05 11:13:50 -0600

    set up Phantom check

--- src/App.tsx
+++ src/App.tsx
@@ -2,56 +2,18 @@ import React, { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 import { getProvider } from './utils'
+import NoPhantom from './components/NoPhantom'
 
 // Constants
 const provider = getProvider();
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
-
-const isPhantomInstalled = window
-// const isPhantomInstalled = window.phantom?.solana?.isPhantom
-// const getProvider = () => {
-//   if ('phantom' in window) {
-//     const provider = window.phantom?.solana;
-
-//     if (provider?.isPhantom) {
-//       return provider;
-//     }
-//   }
-
-//   window.open('https://phantom.app/', '_blank');
-// };
 const App = () => {
-  
-  const checkIfWalletIsConnected = async () => {
-    try {
-      const { solana } = window;
 
-      if (solana) {
-        if (solana.isPhantom) {
-          console.log(isPhantomInstalled)
-          console.log('Phantom wallet found!');
-        }
-      } else {
-        alert('Solana object not found! Get a Phantom Wallet 👻');
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  /*
-   * When our component first mounts, let's check to see if we have a connected
-   * Phantom Wallet
-   */
-  useEffect(() => {
-    const onLoad = async () => {
-      await checkIfWalletIsConnected();
-    };
-    window.addEventListener('load', onLoad);
-    return () => window.removeEventListener('load', onLoad);
-  }, []);
+  if (!provider) {
+    return <NoPhantom />;
+  }
 
   return (
     <div className="App">
