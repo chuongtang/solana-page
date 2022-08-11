f9540ea08801b2fabcc6b91fd2d09ba94918f002
chuongtang <chuong.tang@aol.com>
2022-08-11 11:53:14 -0600

    ✔RspLy set up NoPhantom page 👍

--- src/components/NoPhantom.tsx
+++ src/components/NoPhantom.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import 'animate.css';
+import Solana from '../assets/Solana.svg'
 
 const NoPhantom = () => {
   return (
@@ -6,11 +8,12 @@ const NoPhantom = () => {
       <div className="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
         <div className="max-w-3xl mx-auto text-center">
           <h1 className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-            Hi, thanks for stopping by.
+            Hi, thanks for visting! 
 
-            <span className="sm:block">
-              This is my dApp on Solana blockchain.
+            <span className="sm:block m-2">
+              This is my dApp on <img className="animate-pulse w-18 mx-auto m-8" src={Solana} alt="Solana Logo" /> blockchain.
             </span>
+            
           </h1>
 
           <p className="flex max-w-xl mx-auto justify-center mt-4 sm:leading-relaxed sm:text-xl">
@@ -18,12 +21,12 @@ const NoPhantom = () => {
           </p>
 
           <div className="flex flex-wrap justify-center gap-4 mt-8">
-            <a className="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded sm:w-auto active:text-opacity-75 hover:bg-transparent hover:text-white focus:outline-none focus:ring" href="https://phantom.app/download"
+            <a className="borderAnimation block w-full px-12 py-3 text-sm font-medium rounded sm:w-auto active:text-opacity-75 hover:bg-transparent hover:text-white focus:outline-none focus:ring " href="https://phantom.app/download"
             target="_blank">
               Install Phantom
             </a>
 
-            <a className="block w-full px-12 py-3 text-sm font-medium text-white border border-blue-600 rounded sm:w-auto hover:bg-blue-600 active:bg-blue-500 focus:outline-none focus:ring" href="/about">
+            <a className="block w-full px-12 py-3 text-sm font-medium text-white border-4 border-blue-600 rounded sm:w-auto hover:bg-blue-600 active:bg-blue-500 focus:outline-none focus:ring" href="/about">
               Learn More
             </a>
           </div>
